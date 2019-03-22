1e3cd79e8674b5de4f9b7efa7b415221562b51b6
StarryNight00 <cisa.matias@gmail.com>
2019-03-22 11:53:59 +0000

    exercicio NPCPerks - arrays, enum...

---
+++ NPCPerks/CarateristicasNPC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPCPerks
+{
+    [Flags]
+    enum CarateristicasNPC
+    {
+        Stealth = 1 << 0, //0001
+        Combat = 1 << 1, //0010
+        Lockpick = 1 << 2, //0100
+        Luck = 1 << 3 //1000
+    }
+}
---
+++ NPCPerks/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace NPCPerks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variáveis
+            int NumNpc;
+            string input, input2;
+
+            //declarar Array (referência à enumeração)
+            CarateristicasNPC[] caract;
+
+            //Pedir input para gerar array
+            Console.WriteLine("Quantos NPCs deseja gerar?");
+            input = Console.ReadLine();
+            NumNpc = Convert.ToInt32(input);
+         
+            //criar Array
+            caract = new CarateristicasNPC[NumNpc];
+
+            //Para cada NPC, pedir caracteristicas
+            for (int i = 0; i < NumNpc; i++)
+            {
+                //Perguntar se é stealth
+                Console.WriteLine($"\n\nDeseja que NPC {i+1} tenha o atributo stealth? y/n");
+                input2 = Console.ReadLine();
+                //Adicionar stealth à lista
+                if (input2 == "y")
+                {
+                    caract[i] |= CarateristicasNPC.Stealth;
+                }
+
+                //Perguntar se é combat
+                Console.WriteLine($"\nDeseja que NPC {i+1} tenha o atributo combat? y/n");
+                input2 = Console.ReadLine();
+                //Adicionar combat à lista
+                if (input2 == "y")
+                {
+                    caract[i] |= CarateristicasNPC.Combat;
+                }
+
+                //Perguntar se é lockpick
+                Console.WriteLine($"\nDeseja que NPC {i+1} tenha o atributo lockpick? y/n");
+                input2 = Console.ReadLine();
+                //Adicionar lockpick à lista
+                if (input2 == "y")
+                {
+                    caract[i] |= CarateristicasNPC.Lockpick;
+                }
+
+                //Perguntar se é luck
+                Console.WriteLine($"\nDeseja que NPC {i+1} tenha o atributo luck? y/n");
+                input2 = Console.ReadLine();
+                //Adicionar luck à lista
+                if (input2 == "y")
+                {
+                    caract[i] |= CarateristicasNPC.Luck;
+                }
+            }
+            
+            //Mostrar Características
+            for (int i = 0; i < NumNpc; i++)
+            {
+                //Mostrar Carateristicas
+                Console.WriteLine($"O NPC {i+1} tem as seguintes caraterísticas: ", caract[i]);
+
+                if ((caract[i] & CarateristicasNPC.Combat) == CarateristicasNPC.Combat && (caract[i] & CarateristicasNPC.Luck) == CarateristicasNPC.Luck)
+                {
+                    Console.WriteLine("You shall win all FIGHTS!!!");
+                }
+            }
+        }
+    }
+}
